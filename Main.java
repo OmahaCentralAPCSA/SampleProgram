02b26ecd6330b711ed84c8529061a1d505d5e9b5
Collin Holmquist <collinholmquist@gmail.com>
2020-08-14 19:41:10 +0000

    added some additional text.

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+      double result = toTheNthPower(4, 9);
+      System.out.println("the result is: " + result); 
+
+  }
+
+  public static double toTheNthPower(double base, int exp) {
+
+      double total = 1; 
+
+      for(int i = 1; i <= exp; i++){
+          total = total * base; 
+      }
+        
+      return total; 
   }
 }
